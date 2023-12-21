1f2308a86b7184df0785ff6f05684e68070d0ffc
ShainiAnil <shainij7@gmail.com>
2023-12-21 13:03:27 +0000

    adds globals.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Todo } from './components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-    
-    
-    </>
-  )
+  return <Todo />
 }
 
 export default App
